ec2a20236a5b18ba271d37a4301597c1e52e5c64
Conor Luddy <conorluddy@gmail.com>
2025-03-14 08:17:11 +0000

    Turbo

---
+++ packages/useFuzzyFilter/jest.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./useFuzzyFilter";
-export * from "./useScrollTracker";
