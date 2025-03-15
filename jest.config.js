46561a8ed7e15f0fd11303786edf67fcf64868df
Conor Luddy <conorluddy@gmail.com>
2025-03-15 19:54:36 +0000

    Cleaning up

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
-};
